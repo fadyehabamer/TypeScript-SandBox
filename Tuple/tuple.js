005589ea414826f666946fd1be35a5728ac247cf
fadyehabamer <fadyamer45@gmail.com>
2022-01-25 16:20:20 +0200

    tuples

---
+++ Tuple/tuple.js
@@ -0,0 +1,5 @@
+"use strict";
+// exports.__esModule = true;
+var arr = [1, 2, 3, 4, 5, 6, 7];
+// * Tuple : fixed type array ... [ array of specific type ]
+var data = [1, true, "fady"];
---
+++ Tuple/tuple.ts
@@ -0,0 +1,6 @@
+export {};
+
+let arr:number[] =[1,2,3,4,5,6,7]
+
+// * Tuple : fixed type array ... [ array of specific type ]
+let data:[number,boolean,string] =[1,true,"fady"] 
