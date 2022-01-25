d2ac87cca1ed29b217580f60c1c9c9e30afc4995
fadyehabamer <fadyamer45@gmail.com>
2022-01-25 16:19:37 +0200

    introduction

---
+++ Introduction/intro.ts
@@ -0,0 +1,10 @@
+// * to install TypeScript
+// * npm i -g typescript
+
+
+// * compile TS file using command
+// * tsc "tsc-file name"
+// * will compile this file to file.js in the same directory
+// * import the js file in HTML CODE
+
+// * Browser dont understand TS
