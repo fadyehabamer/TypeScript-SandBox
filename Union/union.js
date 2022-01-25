ca42b790588e10b2eca72fa5294db0ac0a3011d6
fadyehabamer <fadyamer45@gmail.com>
2022-01-25 16:20:35 +0200

    union

---
+++ Union/union.js
@@ -0,0 +1,9 @@
+"use strict";
+// exports.__esModule = true;
+// * union
+// * Variable accept diffrent data types
+// * any   -> any type
+// * union -> specific type
+var x;
+x = 1;
+x = "fady";
---
+++ Union/union.ts
@@ -0,0 +1,11 @@
+export {}
+// * union
+// * Variable accept diffrent data types
+
+// * any   -> any type
+// * union -> specific type
+
+let x: number | string;
+
+x = 1;
+x = "fady";
