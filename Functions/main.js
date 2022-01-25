b8ca4af33ce3ed6ecce1c75b50f8d2cd2b65a768
fadyehabamer <fadyamer45@gmail.com>
2022-01-25 16:21:40 +0200

    Functions

---
+++ Functions/main.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+// * function that take string as a prameter and return value as string
+function displayName(name) {
+    // console.log(name);
+    return name;
+}
+// * function that take number as a prameter and doesn't return value
+function someLogicFunction(num) {
+    // console.log(name);
+    if (num > 10) {
+        console.log("WOW");
+    }
+}
---
+++ Functions/main.ts
@@ -0,0 +1,16 @@
+export {};
+
+// * function that take string as a prameter and return value as string
+function displayName(name: string): string {
+  // console.log(name);
+  return name;
+}
+
+// * function that take number as a prameter and doesn't return value
+function someLogicFunction(num:number): void {
+    // console.log(name);
+    if (num  > 10){
+        console.log("WOW")
+    }
+  }
+  
