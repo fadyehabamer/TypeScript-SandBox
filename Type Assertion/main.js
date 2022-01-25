c49a11ec10f995ab50224ee01a63a1747b64e520
fadyehabamer <fadyamer45@gmail.com>
2022-01-25 16:21:27 +0200

    type assertion

---
+++ Type Assertion/main.js
@@ -0,0 +1,4 @@
+var x = 1;
+// * make other variablr = number [x] with 2 define ways
+var y = x;
+var z = x;
---
+++ Type Assertion/main.ts
@@ -0,0 +1,7 @@
+export {};
+
+let x: any = 1;
+
+// * make other variablr = number [x] with 2 define ways
+let y = <number>x;
+let z = x as number;
